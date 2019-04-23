e44749389624508ac3bc57285ccfcc49f593ba3b
minwook <minwook@teruten.com>
2019-04-23 20:23:06 +0900

    검색 리스트에서 각각의 url 주소 추출 - 진행중

--- chosunCrawling.py
+++ chosunCrawling.py
@@ -2,6 +2,20 @@ from bs4 import BeautifulSoup
 from requests import get
 from urllib.parse import unquote
 
+urlList = []
+
+def chosunAticleList(url):
+
+    testUrl = "http://search.chosun.com/search/news.search?query=%22%EB%B0%9C%EB%8B%AC%EC%9E%A5%EC%95%A0%22+%ED%95%99%EC%83%9D&pageno=0&orderby=news&naviarraystr=&kind=&cont1=&cont2=&cont5=&categoryname=%EC%A1%B0%EC%84%A0%EC%9D%BC%EB%B3%B4&categoryd2=&c_scope=navi&sdate=&edate=&premium="
+
+    rq = get(testUrl)
+    soup = BeautifulSoup(rq.content.decode('utf-8', 'replace'), 'html.parser')
+    contents = soup.find_all(class_='search_news')
+    for item in contents:
+        dtTag = item.find('dt')
+        aTag = dtTag.find('a')
+        print(str(aTag["href"]))
+        urlList.append(str(aTag["href"]))
 
 def getData(url):
     request = get(url)
@@ -34,3 +48,5 @@ print(getData(url1))
 
 print('------------------')
 print(getData(url2))
+print('------------------')
+chosunAticleList("")
