497000ad4fe4dbbdeff310c6681cd91c3a2b39b1
minwook <minwook@teruten.com>
2019-04-26 16:09:41 +0900

    - 검색어, 시작날짜, 끝날짜 입력\n- 조선일보 1차 크롤링 완료

--- chosunCrawling.py
+++ chosunCrawling.py
@@ -1,14 +1,34 @@
 from bs4 import BeautifulSoup
 from requests import get
 from urllib.parse import unquote
+from urllib.parse import quote
+from datetime import datetime
+
+from openpyxl import Workbook
 
 urlList = []
+write_wb = Workbook()
 
-def chosunAticleList(url):
+def chosun_aticle_list(query,s_date,e_date):
+    pageCount = 1
+    queryString = quote(query)
+    #queryString = "%22%EB%B0%9C%EB%8B%AC%EC%9E%A5%EC%95%A0%22%20%ED%95%99%EC%83%9D"
 
-    testUrl = "http://search.chosun.com/search/news.search?query=%22%EB%B0%9C%EB%8B%AC%EC%9E%A5%EC%95%A0%22+%ED%95%99%EC%83%9D&pageno=0&orderby=news&naviarraystr=&kind=&cont1=&cont2=&cont5=&categoryname=%EC%A1%B0%EC%84%A0%EC%9D%BC%EB%B3%B4&categoryd2=&c_scope=navi&sdate=&edate=&premium="
+    while True:
+        searchUrl = "http://search.chosun.com/search/news.search?query=" + queryString + "&pageno=" + str(pageCount) + "&orderby=news&naviarraystr=&kind=&cont1=&cont2=&cont5=&categoryname=%EC%A1%B0%EC%84%A0%EC%9D%BC%EB%B3%B4&categoryd2=&c_scope=&sdate=" + s_date + "&edate=" + e_date + "&premium="
+        pageContainItemCount = get_aticle_list(searchUrl)
+        print("searchUrl : "+searchUrl)
+        print("pageCount : "+str(pageCount))
+        print("pageContainItemCount : "+str(pageContainItemCount))
+        pageCount += 1
+        if pageContainItemCount <= 0:
+            break
 
-    rq = get(testUrl)
+    print("list total count : "+str(len(urlList)))
+
+
+def get_aticle_list(url):
+    rq = get(url)
     soup = BeautifulSoup(rq.content.decode('utf-8', 'replace'), 'html.parser')
     contents = soup.find_all(class_='search_news')
     for item in contents:
@@ -17,7 +37,11 @@ def chosunAticleList(url):
         print(str(aTag["href"]))
         urlList.append(str(aTag["href"]))
 
-def getData(url):
+    return len(contents)
+
+
+
+def get_data(url):
     request = get(url)
     soup = BeautifulSoup(request.content.decode('utf-8', 'replace'), 'html.parser')
 
@@ -26,27 +50,43 @@ def getData(url):
     print("dateStr : "+dateStr)
 
     title = soup.find(attrs={'id': 'news_title_text_id'})
-    contents = soup.find_all(class_='par')
+    contentsTag = soup.find_all(class_='par')
     link = url
     print("title : "+title.text)
 
-    result = ""
-    for div in contents:
-        if result:
-            result = result + " " + div.text
+    contentsString = ""
+    for div in contentsTag:
+        if contentsString:
+            contentsString = contentsString + " " + div.text
         else:
-            result = div.text
+            contentsString = div.text
 
-    return result
+    write_ws.append([dateStr, title.text, "조선일보", contentsString, url])
 
+    return contentsString
 
-url1 = "http://news.chosun.com/site/data/html_dir/2018/05/30/2018053001715.html"
-url2 = "http://news.chosun.com/site/data/html_dir/2018/03/28/2018032800188.html"
+query = input("검색어 입력: ")
+s_date = input("시작날짜 입력(2019.01.04):")  # 2019.01.04
+e_date = input("끝날짜 입력(2019.01.05):")  # 2019.01.05
+
+# Sheet1에다 입력
+write_ws = write_wb.active
+
+# 행 단위로 추가
+write_ws.append(["date", "title", "source", "contents", "link"])
 
 print('------------------')
-print(getData(url1))
+chosun_aticle_list(query,s_date,e_date)
+
+for urlItem in urlList:
+    get_data(urlItem)
 
 print('------------------')
-print(getData(url2))
-print('------------------')
-chosunAticleList("")
+
+# 엑셀로 저장하기 위한 변수
+RESULT_PATH = 'D:/'  # 결과 저장할 경로
+now = datetime.now()  # 파일이름 현 시간으로 저장하기
+# 새로 만들 파일이름 지정
+outputFileName = '%s-%s-%s  %s시 %s분 %s초 merging.xlsx' % (
+    now.year, now.month, now.day, now.hour, now.minute, now.second)
+write_wb.save(RESULT_PATH + outputFileName)
