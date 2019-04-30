fb5ed3daf3a84eb5dad9d336a7f0a008d6065a3a
minwook <minwook@teruten.com>
2019-04-30 20:37:24 +0900

    - 네이버 기사 크롤링 진행중 - 엑셀 저장시 cell의 값에 ' ' 가 들어있으면 에러 수정중

--- chosunCrawling.py
+++ chosunCrawling.py
@@ -1,7 +1,12 @@
+import time
+from random import randint
+
 from bs4 import BeautifulSoup
 from requests import get
+from requests import Session
 from urllib.parse import unquote
 from urllib.parse import quote
+from urllib.request import FancyURLopener
 from datetime import datetime
 
 from openpyxl import Workbook
@@ -9,13 +14,13 @@ from openpyxl import Workbook
 urlList = []
 write_wb = Workbook()
 
-def chosun_aticle_list(query,s_date,e_date):
+def chosun_aticle_list(query):
     pageCount = 1
     queryString = quote(query)
     #queryString = "%22%EB%B0%9C%EB%8B%AC%EC%9E%A5%EC%95%A0%22%20%ED%95%99%EC%83%9D"
 
     while True:
-        searchUrl = "http://search.chosun.com/search/news.search?query=" + queryString + "&pageno=" + str(pageCount) + "&orderby=news&naviarraystr=&kind=&cont1=&cont2=&cont5=&categoryname=%EC%A1%B0%EC%84%A0%EC%9D%BC%EB%B3%B4&categoryd2=&c_scope=&sdate=" + s_date + "&edate=" + e_date + "&premium="
+        searchUrl = "http://search.chosun.com/search/news.search?query=" + queryString + "&pageno=" + str(pageCount) + "&orderby=news&naviarraystr=&kind=&cont1=&cont2=&cont5=&categoryname=%EC%A1%B0%EC%84%A0%EC%9D%BC%EB%B3%B4&categoryd2=&c_scope=&sdate=&edate=&premium="
         pageContainItemCount = get_aticle_list(searchUrl)
         print("searchUrl : "+searchUrl)
         print("pageCount : "+str(pageCount))
@@ -43,7 +48,24 @@ def get_aticle_list(url):
 
 def get_data(url):
     request = get(url)
-    soup = BeautifulSoup(request.content.decode('utf-8', 'replace'), 'html.parser')
+    print("html1 : "+str(request.content))
+
+    rand_value = randint(1, 5)
+    print("rand_value : "+str(rand_value))
+    time.sleep(rand_value)
+
+
+    session = Session()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+    }
+
+    html = session.get(url, headers=headers).content
+    print("html2 : "+str(html))
+
+    soup = BeautifulSoup(html, "html.parser", from_encoding="utf-8")
+    #soup = BeautifulSoup(request.content, "html.parser", from_encoding="utf-8")
 
     date = soup.find(class_='news_date')
     dateStr = date.text.replace('입력 ', '')
@@ -65,23 +87,24 @@ def get_data(url):
 
     return contentsString
 
-query = input("검색어 입력: ")
-s_date = input("시작날짜 입력(2019.01.04):")  # 2019.01.04
-e_date = input("끝날짜 입력(2019.01.05):")  # 2019.01.05
+#query = input("검색어 입력: ")
 
 # Sheet1에다 입력
 write_ws = write_wb.active
 
 # 행 단위로 추가
 write_ws.append(["date", "title", "source", "contents", "link"])
-
+'''
 print('------------------')
 chosun_aticle_list(query,s_date,e_date)
 
 for urlItem in urlList:
     get_data(urlItem)
-
+'''
 print('------------------')
+
+
+get_data("http://news.chosun.com/svc/content_view/content_view.html?contid=2001031270234")
 
 # 엑셀로 저장하기 위한 변수
 RESULT_PATH = 'D:/'  # 결과 저장할 경로
@@ -89,4 +112,4 @@ now = datetime.now()  # 파일이름 현 시간으로 저장하기
 # 새로 만들 파일이름 지정
 outputFileName = '%s-%s-%s  %s시 %s분 %s초 merging.xlsx' % (
     now.year, now.month, now.day, now.hour, now.minute, now.second)
-write_wb.save(RESULT_PATH + outputFileName)
+#write_wb.save(RESULT_PATH + outputFileName)
